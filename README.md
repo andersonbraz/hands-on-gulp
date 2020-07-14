6beb6377e5153b1413b59caf0d5600663cafe4f6
Anderson Braz <braz_anderson@msn.com>
2020-07-13 22:20:52 -0300

    Up Files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# hands-on-gulp
+# Hands-on  With Gulp
+
 Hands-on with Gulp - Gulp é um kit de ferramentas Javascript.
+
+[EXAMPLE]("https://semaphoreci.com/community/tutorials/getting-started-with-gulp-js")
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+
+gulp.task('dev', function(){
+    console.log("Development mode ...");
+});
+
+gulp.task('prod', function(){
+    console.log("Prodution mode ...");
+});
+
+gulp.task('default',function(){
+    
+});
+
